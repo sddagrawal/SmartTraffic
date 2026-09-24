37ed3d25e65400e276ee4d24d45109171f1d0347
SIDDHI AGRAWAL <sddagrawal@gmail.com>
2026-09-24 16:45:16 +0530

    updated smart traffic signal system

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-#road class def
-class RoadDirection:
+#begin
+def calculate_priority(road):
+    emergency_bonus =100 if road["emergency"] else 0
+    priority_score=(road["vehicles"]*2)+(road["wait_time"]*0.5)+ emergency_bonus 
+    return priority_score
 
-    def __init__(self,dirc,vehicles,wait_time,emergency):
-        self.dirc=dirc
-        self.vehicles=vehicles
-        self.wait_time=wait_time
-        self.emergency=emergency
-
-    def calculate_priority(self):
-        emergency_bonus =100 if self.emergency else 0
-        priority_score=((self.vehicles*2)+(self.wait_time*0.5)+ emergency_bonus )
-        return priority_score
-
-    def check_congestion(self):
-        #if vehicle number or wait time exceeds threshold
-        if self.vehicles>20 or self.wait_time>60:
-            return "congested"
-        else:
-             return "normal"
+def check_congestion(road):
+    #if vehicle number or wait time exceeds threshold
+    if road["vehicles"]>20 or road["wait_time"]>60:
+        return "congested"
+    else:
+            return "normal"
 
 #1
 def get_traffic_data():
@@ -27,15 +19,12 @@ def get_traffic_data():
     print("\n--- Enter traffic data for each direction ---")
     
     for dirc in directions:
-        print(f"\n>>> Input for {dirc} directiomn:")
+        print(f"\n>>> Input for {dirc} direction:")
         
         #(1) valid vehicle count
         while True:
             try:
                 vehicles= int (input(f"enter number of vehicles in {dirc}:"))
-                if vehicles<0:
-                    print("count can not be negative!!")
-                    continue
                 break
             except ValueError:
                 print("enter valid numeric value")
@@ -44,9 +33,6 @@ def get_traffic_data():
         while True:
             try:
                 wait_time=float(input(f"enter waiting time (sec) for {dirc}"))
-                if wait_time<0:
-                    print("wait time can not be negative!!")
-                    continue
                 break
             except ValueError:
                 print ("please enter a valid number as input")
@@ -63,44 +49,21 @@ def get_traffic_data():
             else:
                 print("invalid input!! please type 'yes' or 'no'")
         
-        #creating a RoadDirection object and storing it
-        road = RoadDirection(dirc,vehicles,wait_time,emergency)
-        road_objects.append(road)
+        #storing data 
+        road_objects.append({"dirc": dirc, "vehicles": vehicles, "wait_time":wait_time, "emergency":emergency})
     return road_objects
-
-"""#testing 
-if __name__=="__main__":
-    traffic_data= get_traffic_data()
-
-    print("\n--- collected data ---")
-    for road in traffic_data:
-        print(f"{road.dirc}:{road.vehicles} vehicles, {road.wait_time}s wait, emergency: {road.emergency}")
-        """
-
-
-"""#testing 
-if __name__== "__main__":
-    traffic_data= get_traffic_data()
-
-    print ("/n--- priority and congestion ---")
-    for road in traffic_data:
-        score= road.calculate_priority()
-        status=road.check_congestion()
-        print(f"{road.dirc} : priority score= {score}, status={status}")"""
-
-
 
 #2
 def allocate_green_times(road_list, total_cycle_time=120, min_time=10):
     #calculating priority score
-    total_priority= sum(road.calculate_priority() for road in road_list)
+    total_priority= sum(calculate_priority(road) for road in road_list)
 
     remaining_pool= total_cycle_time-(min_time*len(road_list))
 
     alloacted_times={}
 
     for road in road_list:
-        score= road.calculate_priority()
+        score= calculate_priority(road)
         if total_priority>0:
             extra_time=(score/ total_priority)* remaining_pool
             green_time= min_time+ extra_time
@@ -110,84 +73,65 @@ def allocate_green_times(road_list, total_cycle_time=120, min_time=10):
             green_time=total_cycle_time/ len (road_list)
 
         #rounding off
-        alloacted_times[road.dirc]= round(green_time)
+        alloacted_times[road["dirc"]]= round(green_time)
 
     return alloacted_times
 
 def get_signal_schedule (road_list):
     #sorting priority using lambda
-    sorted_roads= sorted(road_list, key=lambda road:road.calculate_priority() , reverse=True)
+    sorted_roads= sorted(road_list, key=lambda road:calculate_priority(road) , reverse=True)
     return sorted_roads
 
-"""#testing 
-if __name__= "__main__":
-    traffic_data= get_traffic_data()
-
-    #for green light
-    green_times= alloacted_times(traffic_data)
-    #for order of traffic signals
-    scheduled_roads= get_signal_schedule(traffic_data)
-
-    print("\n--- signal schedule and allocation ---")
-    for rank, road in enumerate (scheduled_roads:1):
-        dirc_name= road.dirc.capitalize()
-        priority= road.calculate_priority()
-        allocated_sec= green_times[road.dirc]
-        emergency_tag= "[emergency]" if road.emergency else ""
-
-        print (f"Order{rank}: {dirc_name} {emergency_tag} --> priority: {priority} --> green time : {allocated_sec} s")"""
     
         
 #3
 def calculate_performance(road_list, alloacted_times, standard_fixed_time=30):
-    fixed_total_wait= sum (road.vehicles*road.wait_time for road in road_list)
+    fixed_total_wait= sum (road["vehicles"]*road["wait_time"] for road in road_list)
 
     if fixed_total_wait==0:
         return 0.0,0.0,0.0
 
     optimized_total_wait=0
     for road in road_list:
-        green= alloacted_times[road.dirc]
+        green= alloacted_times[road["dirc"]]
         time_factor= standard_fixed_time/ green if green>0 else 1.0 
-        optimized_total_wait+=road.vehicles*(road.wait_time*time_factor)
+        optimized_total_wait+=road["vehicles"]*(road["wait_time"]*time_factor)
 
     improvement=((fixed_total_wait- optimized_total_wait)/ fixed_total_wait)*100
     return(round (fixed_total_wait, 1),round(optimized_total_wait,1),round (improvement,2),)
 
 #4
-if __name__ == "__main__":
-    while True:
-        traffic_data= get_traffic_data()
+while True:
+    traffic_data= get_traffic_data()
 
-        green_times= allocate_green_times(traffic_data)
-        scheduled_roads= get_signal_schedule(traffic_data)
+    green_times= allocate_green_times(traffic_data)
+    scheduled_roads= get_signal_schedule(traffic_data)
 
-        fixed_wait, opt_wait, improvement = calculate_performance(traffic_data, green_times)
+    fixed_wait, opt_wait, improvement = calculate_performance(traffic_data, green_times)
 
-        print ("#SMART TRAFFIC SIGNAL FINAL REPORT#")
+    print ("#SMART TRAFFIC SIGNAL FINAL REPORT#")
 
-        print("\n ---- signal order and allocation ----")
-        for rank, road in enumerate (scheduled_roads,1):
-            dirc_name= road.dirc.capitalize()
-            priority=road.calculate_priority()
-            status=road.check_congestion()
-            emergency_tag= "[EMERGENCY]" if road.emergency else ""
-            
-            allocated_sec = green_times[road.dirc]
-            print(f"order {rank}: {dirc_name:<7}{emergency_tag} --> priority: {priority:<5} --> status: {status:<9} --> green time: {allocated_sec}s")
+    print("\n ---- signal order and allocation ----")
+    for rank, road in enumerate (scheduled_roads,1):
+        dirc_name= road["dirc"].capitalize()
+        priority=calculate_priority(road)
+        status=check_congestion(road)
+        emergency_tag= "[EMERGENCY]" if road["emergency"] else ""
         
-        print("\n--- system performance comparison ---")
-        print(f"fixed system total wait delay: {fixed_wait}s")
-        print(f"optimized system total wait : {opt_wait}s")
-        print(f"efficiency improvement: {improvement}%")
+        allocated_sec = green_times[road["dirc"]]
+        print(f"order {rank}: {dirc_name:<7}{emergency_tag} --> priority: {priority:<5} --> status: {status:<9} --> green time: {allocated_sec}s")
+        
+    print("\n--- system performance comparison ---")
+    print(f"fixed system total wait delay: {fixed_wait}s")
+    print(f"optimized system total wait : {opt_wait}s")
+    print(f"efficiency improvement: {improvement}%")
+ #5
+    repeat= (input ("\n do you want to run another simulation? yes/no:").strip().lower())
+    if repeat not in ["yes","y"]:
+        print("\nexiting")
+        break
 
-        #5
-        repeat= (input ("\n do you want to run another simulation? yes/no:").strip().lower())
-        if repeat not in ["yes","y"]:
-            print("\nexiting")
-            break
-        
-        
+    
 
 
         
